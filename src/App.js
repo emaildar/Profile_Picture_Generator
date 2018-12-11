f2639220d78a9e981c37385fac0b005158f77df0
emaildarlyn <emaildarlyn@gmail.com>
2018-12-11 15:16:33 +0700

    upload Profile Picture Generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
+  state = { forminput: "", 
+            cetak: "" 
+          }
+
+    gambar = () => {
+        this.setState({
+            cetak: this.state.forminput
+        })
+    }
+
+    display() {
+        return (
+            <p><img style={{borderRadius: "50%", backgroundColor: 'white', width: '200px', marginTop: '30px'}} src={`https://robohash.org/${this.state.cetak}.png`} alt="404" /></p> 
+        )
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container text-center p-5">
+          <input type="text" className="form-control w-25 mx-auto my-3" placeholder="User Name" onChange={(x) => { this.setState({ forminput: x.target.value }) }}></input>
+          <button type="button" className="btn btn-success" onClick={this.gambar}>Buat Profile Picture!</button>
+          <div className="row">
+              <div className="col-lg-12 text-center">
+                  {this.state.cetak && this.display()}
+              </div>
+          </div>
       </div>
     );
   }
